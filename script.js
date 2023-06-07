6abe2b9dae8688dd870275d7064996665a9bc9eb
szymon-kalinowski <szymonkalinowski1@gmail.com>
2023-06-07 12:43:51 +0200

    trying with object

---
+++ script.js
@@ -0,0 +1,7 @@
+const randomData = {
+    names: ['szymon', 'zbigniew', 'tomek', 'agnieszka', 'damian', 'marta', 'bora'],
+    future: ['today', 'tommorow', 'in close future'],
+    fortune: ['you will be lucky', 'you will have bad luck', 'you will get another fortune cookie', 'you will go for a walk into forrest']
+}
+// this objesct keeps data for our code
+
