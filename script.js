7d13524ecc3347dfea83c7a0b898ab4fe9e4dddb
szymon-kalinowski <szymonkalinowski1@gmail.com>
2023-06-07 13:46:22 +0200

    final product

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 const randomData = {
-    names: ['szymon', 'zbigniew', 'tomek', 'agnieszka', 'damian', 'marta', 'bora'],
+    names: ['Szymon', 'Zbigniew', 'Tomek', 'Agnieszka', 'Damian', 'Marta', 'Bora'],
     future: ['today', 'tommorow', 'in close future'],
     fortune: ['you will be lucky', 'you will have bad luck', 'you will get another fortune cookie', 'you will go for a walk into forrest']
 }
 // this objesct keeps data for our code
 
+function getRandomString(arr) {
+    const randomIndex = Math.floor(Math.random() * arr.length);
+    const string = arr[randomIndex];
+    return string;
+}
+//console.log(getRandomString(randomData.names))
+const randomName = getRandomString(randomData.names);
+
+const randomFuture = getRandomString(randomData.future);
+
+const randomFortune = getRandomString(randomData.fortune);
+
+const fortuneCookieParty = `${randomName} ${randomFuture} ${randomFortune}.`
+console.log(fortuneCookieParty)
